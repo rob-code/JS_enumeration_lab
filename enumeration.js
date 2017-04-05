c8cc25642e34141ce63e6a170492fe2b6bb3d3fc
Robert Brice <rob.a.brice@gmail.comC>
2017-04-05 16:09:44 +0100

    passing first 2 tests

---
+++ enumeration.js
@@ -0,0 +1,52 @@
+// Your task is to write your own functions which implement the following built-in enumeration methods:
+
+// * `find()`
+// * `map()`
+// * `filter()`
+// * `some()`
+// * `every()`
+
+// Bonus points:
+
+// * `reduce()`
+// * Write your own `forEach()` function and use it in your other enumeration methods. (There is no test for this!)
+
+// You will be given start code and some tests ( `./specs/enumeration_specs.js` ) and all you have to do is make them pass!
+
+var Enumeration = function() {}
+
+Enumeration.prototype = {
+
+  find: function(array, callback){
+    
+    for (var i = 0; i < array.length; i++){
+        var trueOrFalse = callback(array[i]);
+        if (trueOrFalse) {
+          return array[i];
+        }
+    }    
+
+      return undefined;
+
+  },
+
+  map: function(array, callback){
+    var newArray = [];
+    for (var i = 0; i < array.length; i++){
+      newArray.push(callback(array[i]));
+    }
+      return newArray
+  },
+
+  filter: function(array, callback){
+
+
+    
+  }
+
+
+
+
+}
+
+module.exports = Enumeration;
